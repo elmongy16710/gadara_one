b30de4d80118d54e4b5dbb238b8b86aea88bd7f4
ashraf elmongy <ashraf.elmongy80@gmail.com>
2023-07-17 15:08:05 +0300

    index

--- js/main.js
+++ js/main.js
@@ -2,8 +2,6 @@ const swiper = new Swiper('.swiper', {
     // Optional parameters
     direction: 'horizontal',
 
-
-
     // If we need pagination
     pagination: {
         el: '.swiper-pagination',
@@ -23,9 +21,47 @@ const swiper = new Swiper('.swiper', {
 
 
 
+// header-slider
+
 let myBollets = [...document.querySelectorAll(".custom-swiper-pagination span")];
 
+let myAllItems = document.querySelector(".custom-swiper-wrapper");
 
-for(let i = 0 ; i < 10; i++){
+
+
+
+for (let i = 0; i < myAllItems.children.length; i++) {
     myBollets[i].innerHTML = `0${i + 1}`
 }
+
+
+
+const swiper3 = new Swiper('.Management-swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+
+
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+    breakpoints: {
+        // when window width is >= 320px
+      
+        // when window width is >= 480px
+        
+       
+    }
+});
